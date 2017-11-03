256eedcc9b7bb74ca0894f74424e4a897b400408
Jay Boehm <jayrox@gmail.com>
2017-11-03 09:50:01 -0400

    Update mu.bat

--- mu.bat
+++ mu.bat
@@ -1,5 +1,5 @@
-for /L %%n in (1 1 1) do (
-    sm http://__RADARR__HOST__:__RADARR__PORT__/radarr/api/movie/%%n?apikey=__RADARR__API__KEY__
+for /L %%n in (1 4000 1) do (
+    mu http://__RADARR__HOST__:__RADARR__PORT__/radarr/api/movie/%%n?apikey=__RADARR__API__KEY__
 )
 
 pause
